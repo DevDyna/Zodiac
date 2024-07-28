5bfd315af95155468866004b62cf1797f144eaf7
DevDyna <andrearisorti2003@gmail.com>
2024-07-29 00:20:12 +0200

    Update carrots.js

--- kubejs/startup_scripts/carrots.js
+++ kubejs/startup_scripts/carrots.js
@@ -5,9 +5,9 @@ StartupEvents.registry("block", (event) => {
     .create("kubejs:carrot", "crop")
     .age(3, (builder) => {
       builder
-         .shape(0, 5, 0, 5, 11, 12, 11)
-         .shape(1, 5, 0, 5, 11, 12, 11)
-         .shape(2, 5, 0, 5, 11, 12, 11)
+         .shape(0, 5, 0, 5, 11, 6, 11)
+         .shape(1, 5, 0, 5, 11, 8, 11)
+         .shape(2, 5, 0, 5, 11, 10, 11)
          .shape(3, 5, 0, 5, 11, 12, 11)
     })
     .survive((state, level, pos) => {
