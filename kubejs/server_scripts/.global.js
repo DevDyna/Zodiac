10c7d38020e56f7ba67824aa0194c3ea488f52c9
DevDyna <andrearisorti2003@gmail.com>
2024-07-31 19:11:43 +0200

    Update .global.js

--- kubejs/server_scripts/.global.js
+++ kubejs/server_scripts/.global.js
@@ -1,20 +1,20 @@
 // priority: 1
 function rnd(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  function rnd50() {
-    return Math.random() < 0.5;
-  }
-  
-  function rnd75() {
-    return Math.random() < 0.75;
-  }
-  
-  function rnd25() {
-    return Math.random() < 0.25;
-  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  function rndC(value) {
-    return Math.random() < (0.01 * value);
-  }
+function rnd50() {
+  return Math.random() < 0.5;
+}
+
+function rnd75() {
+  return Math.random() < 0.75;
+}
+
+function rnd25() {
+  return Math.random() < 0.25;
+}
+
+function rndC(value) {
+  return Math.random() < (value == undefined ? 1 : value * 0.01);
+}
