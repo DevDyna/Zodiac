a992dbb2e5b3d31be4944ec87280302237b0c0bf
DevDyna <andrearisorti2003@gmail.com>
2024-08-28 01:37:28 +0200

    Create composter.broken.js

---
+++ kubejs/server_scripts/composter.broken.js
@@ -0,0 +1,9 @@
+BlockEvents.broken("kubejs:composter", (event) => {
+  if (event.block.properties.get("mature").toLowerCase() == "true") {
+    global.jei.recipes.composting[
+      Number(event.block.properties.get("type").toLowerCase())-1
+    ].output.forEach((element) => {
+      event.block.popItem(element);
+    });
+  }
+});
