66b8771b906e7aed42a098b2a78208f0dca40ea2
DevDyna <andrearisorti2003@gmail.com>
2024-08-27 17:32:26 +0200

    Update custom.js

--- kubejs/startup_scripts/custom.js
+++ kubejs/startup_scripts/custom.js
@@ -1,12 +1,27 @@
 StartupEvents.registry("block", (event) => {
+
+  function crate(name,texture){
+    event.create(name).defaultCutout().modelJson = {
+      "parent": "zodiac:block/crate/template_crate",
+      "textures": {
+        "crate_top": texture,
+        "crate_side": texture
+      }
+    }
+  }
+
   //organic dirt -> mud
   event.create("kubejs:muddy_dirt").textureAll("minecraft:block/mud");
   //dry dirt -> sand
   event.create("kubejs:silty_dirt").textureAll("minecraft:block/sand");
   //pebble dirt -> gravel
-  event.create("kubejs:rocky_dirt").textureAll("minecraft:block/gravel");
+  event.create("kubejs:rocky_dirt").textureAll("minecraft:block/coarse_dirt");
 
-  event.create("kubejs:stick_crate").textureAll("minecraft:block/oak_planks");
+  crate("kubejs:crate_of_cobblestone","minecraft:block/cobblestone")
+  crate("kubejs:crate_of_sticks","minecraft:block/oak_planks")
+  crate("kubejs:crate_of_dirt","minecraft:block/dirt")
+  crate("kubejs:crate_of_soil","farmersdelight:block/rich_soil")
+
 });
 
 StartupEvents.registry("item", (event) => {
