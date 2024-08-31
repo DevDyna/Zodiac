89b82446ec5ee22e240f970f32842d592ebf8b6d
DevDyna <andrearisorti2003@gmail.com>
2024-08-31 23:42:50 +0200

    By By Mekanism Fission!

---
+++ kubejs/client_scripts/jei.js
@@ -0,0 +1,28 @@
+JEIEvents.removeCategories((event) => {
+  let categories = [
+    "mekanism:sps_casing",
+    "minecraft:anvil",
+    "minecraft:brewing",
+    "minecraft:compostable",
+    "jei:information",
+    "railcraft:fluid_boiler",
+    "railcraft:solid_boiler",
+    "ae2:attunement",
+    "ae2:certus_growth",
+    "ae2:condenser",
+    "create:automatic_brewing",
+    "create:automatic_packing",
+    "create:automatic_shaped",
+    "create:automatic_shapeless",
+    "create:draining",
+    "create:item_application",
+    "create:mystery_conversion",
+    "create:spout_filling",
+    "terraqueous:endertable",
+    "mekanism:boiler_casing",
+    "mekanism:nutritional_liquifier",
+  ];
+  categories.forEach((e) => {
+    event.remove(e);
+  });
+});
