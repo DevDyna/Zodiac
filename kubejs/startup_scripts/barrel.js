bf931584ec7a2b8f0d907e7c66e289d7dea2f6c7
DevDyna <andrearisorti2003@gmail.com>
2024-09-01 00:55:07 +0200

    Update barrel.js

--- kubejs/startup_scripts/barrel.js
+++ kubejs/startup_scripts/barrel.js
@@ -6,5 +6,7 @@ StartupEvents.registry("block", (event) => {
         parent: "zodiac:block/barrel",
       });
     })
+    .box(1, 0, 1, 15, 16, 15) //TODO NEED TO BE MORE CLEAN
+    .defaultCutout()
     .model("zodiac:block/barrel");
 });
