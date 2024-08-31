31362cc6449963951c4874289a7f37cdb8d4c1e4
DevDyna <andrearisorti2003@gmail.com>
2024-09-01 01:47:25 +0200

    Update alpha-stage.js

--- kubejs/server_scripts/alpha-stage.js
+++ kubejs/server_scripts/alpha-stage.js
@@ -10,33 +10,33 @@ ServerEvents.recipes(event=>{
     event.remove({})
 })
 
-ItemEvents.rightClicked((event) => {
-  event.player.sendData("show", { list: global.jei.stage });
-  //event.player.tell("added " + event.item + " on jei");
-  global.jei.stage.push(event.item.id);
-  //event.player.tell(global.jei.stage);
-  //event.player.tell(event.player.stages.all)
-});
+// ItemEvents.rightClicked((event) => {
+//   event.player.sendData("show", { list: global.jei.stage });
+//   //event.player.tell("added " + event.item + " on jei");
+//   global.jei.stage.push(event.item.id);
+//   //event.player.tell(global.jei.stage);
+//   //event.player.tell(event.player.stages.all)
+// });
 
-ServerEvents.tick((event) => {
-    event.server.scheduleInTicks(20,()=>{
-        event.server.players.forEach((e) => {
-            e.sendData("show", { list: global.jei.stage });
-          });
-    })
-});
+// ServerEvents.tick((event) => {
+//     event.server.scheduleInTicks(20,()=>{
+//         event.server.players.forEach((e) => {
+//             e.sendData("show", { list: global.jei.stage });
+//           });
+//     })
+// });
 
-PlayerEvents.loggedIn((event) => {
-  event.player.sendData("show", { list: global.jei.stage });
+// PlayerEvents.loggedIn((event) => {
+//   event.player.sendData("show", { list: global.jei.stage });
   
-});
+// });
 
-PlayerEvents.loggedOut(event=>{
-    //event.server.persistentData.putArr()
-    //jei.push([event.player.uuid,global.jei.stage])
-})
-
-// ItemEvents.rightClicked(event=>{
-//     event.player.sendData('hide',{item:'minecraft:stone'})
-//     event.player.tell('stage add_item removed')
+// PlayerEvents.loggedOut(event=>{
+//     //event.server.persistentData.putArr()
+//     //jei.push([event.player.uuid,global.jei.stage])
 // })
+
+// // ItemEvents.rightClicked(event=>{
+// //     event.player.sendData('hide',{item:'minecraft:stone'})
+// //     event.player.tell('stage add_item removed')
+// // })
