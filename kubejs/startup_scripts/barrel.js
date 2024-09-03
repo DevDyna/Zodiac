6d26cae59762b860311bcb5286c5c21bc05eebe9
DevDyna <andrearisorti2003@gmail.com>
2024-09-03 11:52:57 +0200

    no more barrels around!

--- kubejs/startup_scripts/barrel.js
+++ kubejs/startup_scripts/barrel.js
@@ -3,10 +3,10 @@ StartupEvents.registry("block", (event) => {
     .create("kubejs:barrel")
     .item((item) => {
       item.modelJson({
-        parent: "zodiac:block/barrel",
+        parent: "zodiac:block/barrel/template/empty",
       });
     })
     .box(1, 0, 1, 15, 16, 15) //TODO NEED TO BE MORE CLEAN
     .defaultCutout()
-    .model("zodiac:block/barrel");
+    .model("zodiac:block/barrel/template/empty");
 });
