601f794b8275de1aea6729f5f142e024efca62f6
DevDyna <andrearisorti2003@gmail.com>
2024-09-15 13:19:22 +0200

    updated tooltips

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -1,3 +1,13 @@
-// ItemEvents.tooltip(event=>{
-//     event.add('kubejs:composter','Allow to combine items into blocks')
-// })
+ItemEvents.tooltip((event) => {
+  event.add("kubejs:composter", "Allow to combine items into blocks");
+
+  event.add(["kubejs:water_bottle", "kubejs:limewater_bottle"], "§aNo Potion");
+
+  event.add('watercollector:watercollector','§aGenerate Unlimited Water from nothing')
+
+  event.addAdvanced('minecraft:potion',(item,advanced,text)=>{
+    if(item.nbt == null){
+        text.add(3,'§cERROR 404')
+    }
+  })
+});
